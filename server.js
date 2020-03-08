43e5db796a0ebd100ad6496391a1cf0dc8e1b982
Glitch (hello-express) <none>
2020-03-08 10:04:09 +0000

    👙🖼 Checkpoint ./server.js:585988/568 ./package.json:585988/746

--- server.js
+++ server.js
@@ -1,20 +1,8 @@
-// todo: create socket.io server based on https://github.com/tetra-fox/nowplaying/blob/master/server.js
-
 const axios = require('axios');
-const cron = require('cron');
-
-const express = require('express');
-const app = express();
+const io = require('socket.io')();
 
 const getTrack = () => axios
     .get('https://api.spotify.com/v1/me/player/currently-playing', { headers: { 'Authorization': `Bearer ${process.env.AUTH}` }})
     .then(res => console.log(res.data));
 
-getTrack()
-// new cron.CronJob('0 */1 * * * *',  () => getTrack(), null, true, 'America/Los_Angeles').start();
-
-// express route
-app.get('/', (req, res) => {
-  console.log('/ accessed')
-  res.send(getTrack());
-})
+io.on('connection', socket => socket.emit('track', track));
