eeb7f50fde692e4189e4479a6717a14d01eba9de
Glitch (hello-express) <none>
2020-03-08 12:54:57 +0000

    🏘📻 Checkpoint ./server.js:585988/4275 ./package.json:585988/26

--- server.js
+++ server.js
@@ -1,12 +1,8 @@
-const axios = require('axios');
-const io = require('socket.io')();
-
-// make an express app
-const express = require('express');
-const app = express();
+const axios = require("axios");
+const io = require("socket.io")();
 
 // create spotify client
-const nodespotify = new (require('node-spotify-api'))({
+const nodespotify = new (require("node-spotify-api"))({
   id: process.env.SPOTIFYID,
   secret: process.env.SPOTIFYSECRET
 });
@@ -16,10 +12,19 @@ let track = {};
 
 // get information about the currently scrobbled track
 const getTrack = async () => {
-  // find my most recently/scrobbled track on 
-  const lastfm = await axios.get(`http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=itsnewt&api_key=${process.env.LASTFM}&format=json&limit=1`).then(res => res.data.recenttracks.track[0]);
-  const spotify = await nodespotify.search({ type: 'track', query: `${lastfm.artist['#text']} - ${lastfm.name}` });
+  // find my most recently/scrobbled track on
+  const lastfm = await axios
+    .get(
+      `http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=itsnewt&api_key=${process.env.LASTFM}&format=json&limit=1`
+    )
+    .then(res => res.data.recenttracks.track[0]);
   
+  const spotify = await nodespotify.search({
+    type: "track",
+    query: `${lastfm.artist["#text"]} - ${lastfm.name}`
+  });
+
+  // compile basic information
   track = {
     album: {
       name: spotify.tracks.items[0].album.name,
@@ -29,35 +34,24 @@ const getTrack = async () => {
       name: spotify.tracks.items[0].name,
       url: spotify.tracks.items[0].external_urls.spotify,
       preview: spotify.tracks.items[0].preview_url,
-      nowPlaying: lastfm['@attr'] !== undefined ? true : false                                
+      nowPlaying: lastfm["@attr"] !== undefined ? true : false
     }
-  }
-  
+  };
+
+  // add information about the artists
   track.artists = spotify.tracks.items[0].artists.map(artist => {
     return {
       name: artist.name,
       url: artist.external_urls.spotify
-    }
+    };
   });
 };
 
-// webserver
-
-// make express use 'public' as its static folder
-app.use(express.static('public'));
-
-// https://now-scrobbling.glitch.me/
-app.get('/', (req, res) => {
-  console.log('accessed /')
-  res.sendFile(`${__dirname}/views/index.html`);
-});
-
 // when there is a connection to the websocket, emit the track event
-io.on('connection', socket => socket.emit('track', track));
+io.on("connection", socket => socket.emit("track", track));
 
-// make the websocket listen on the WSPORT, and the express server on the default PORT
-io.listen(process.env.WSPORT);
-app.listen(process.env.PORT).then(() => console.log('tes'));
+// start websocket
+io.listen(process.env.PORT);
 
 // run the track fetcher every 1000 ms
-setInterval(getTrack, 1000);
+setInterval(() => getTrack(), 1000);
