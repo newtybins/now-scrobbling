fabb275072108d7c222d3fdaadb5157ff59a08a3
Glitch (hello-express) <none>
2020-03-08 13:29:26 +0000

    📺🎖 Checkpoint ./server.js:585988/7900

--- server.js
+++ server.js
@@ -7,65 +7,57 @@ const nodespotify = new (require("node-spotify-api"))({
   secret: process.env.SPOTIFYSECRET
 });
 
-// initialise empty object
-let track = {};
-let oldData = {};
+// when there is a connection to the websocket, emit the track event
+io.on("connection", async socket => {
+  // initialise empty object
+  let track = {};
+  let oldData = {};
 
-// get information about the currently scrobbled track
-const getTrack = async () => {
-  // find my most recently/scrobbled track on
-  const lastfm = await axios
-    .get(
-      `http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=itsnewt&api_key=${process.env.LASTFM}&format=json&limit=1`
-    )
-    .then(res => res.data.recenttracks.track[0]);
-  
-  const spotify = await nodespotify.search({
-    type: "track",
-    query: `${lastfm.artist["#text"]} - ${lastfm.name}`
-  });
+  // get information about the currently scrobbled track
+  const getTrack = async () => {
+    if (track === undefined) return;
+    oldData = track;
 
-  // compile basic information
-  track = {
-    album: {
-      name: spotify.tracks.items[0].album.name,
-      url: spotify.tracks.items[0].album.external_urls.spotify
-    },
-    song: {
-      name: spotify.tracks.items[0].name,
-      url: spotify.tracks.items[0].external_urls.spotify,
-      preview: spotify.tracks.items[0].preview_url,
-      nowPlaying: lastfm["@attr"] === undefined ? false : true
-    }
+    // find my most recently/scrobbled track on
+    const lastfm = await axios
+      .get(
+        `http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=itsnewt&api_key=${process.env.LASTFM}&format=json&limit=1`
+      )
+      .then(res => res.data.recenttracks.track[0]);
+
+    const spotify = await nodespotify.search({
+      type: "track",
+      query: `${lastfm.artist["#text"]} - ${lastfm.name}`
+    });
+
+    // compile basic information
+    track = {
+      album: {
+        name: spotify.tracks.items[0].album.name,
+        url: spotify.tracks.items[0].album.external_urls.spotify
+      },
+      song: {
+        name: spotify.tracks.items[0].name,
+        url: spotify.tracks.items[0].external_urls.spotify,
+        preview: spotify.tracks.items[0].preview_url,
+        nowPlaying: lastfm["@attr"] === undefined ? false : true
+      }
+    };
+
+    // add information about the artists
+    track.artists = spotify.tracks.items[0].artists.map(artist => {
+      return {
+        name: artist.name,
+        url: artist.external_urls.spotify
+      };
+    });
   };
 
-  // add information about the artists
-  track.artists = spotify.tracks.items[0].artists.map(artist => {
-    return {
-      name: artist.name,
-      url: artist.external_urls.spotify
-    };
-  });
-};
-
-// run the track fetcher every 1000 ms
-setInterval(() => {
-  oldData = track;
-  console.log(oldData)
-  getTrack();
-  console.log(track)
-}, 1000);
-
-
-// when there is a connection to the websocket, emit the track event
-io.on("connection", socket => { 
-  socket.emit("track", track);
-  
-  // check for new song
-  const checkForNewSong = oldData => oldData.song.name !== track.song.name ? socket.emit('track', track) : null;
-  setInterval(checkForNewSong(), 1000);
-  
-  console.log('new song!')
+  // run the track fetcher every 1000 ms
+  setInterval(async () => {
+    await getTrack();
+    socket.emit('track', track);
+  }, 1000);
 });
 
 // start websocket
