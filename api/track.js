89e5ea08b9d444d0179cac1a7c26b88ad1c74b53
newt <newtykins@outlook.com>
2020-07-30 19:29:02 +0100

    Update the formatting of the response from track

--- api/track.js
+++ api/track.js
@@ -1,43 +1,51 @@
-const axios = require("axios");
+const axios = require('axios');
+const ms = require('ms');
 
-// create spotify client
-const nodespotify = new(require("node-spotify-api"))({
+const nodespotify = new(require('node-spotify-api'))({
     id: process.env.SPOTIFYID,
     secret: process.env.SPOTIFYSECRET
 });
 
 module.exports = async (req, res) => {
-    // find my most recently/scrobbled track on lastfm
+    // get information about the song
     const lastfm = await axios
         .get(
             `http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=${process.env.USERNAME}&api_key=${process.env.LASTFM}&format=json&limit=1`
         )
         .then(res => res.data.recenttracks.track[0]);
 
-    // find more information about the track from spotify
-    const spotify = await nodespotify.search({
-        type: "track",
-        query: `${lastfm.artist["#text"]} - ${lastfm.name}`
-    });
+    const spotify = (await nodespotify.search({
+        type: 'track',
+        query: `${lastfm.artist['#text']} - ${lastfm.name}`
+    })).tracks.items[0];
 
-    // compile basic information
-    // add information about the artists
-    const artists = await spotify.tracks.items[0].artists.map(artist => {
+    // get information about the artists of the song
+    const artists = await spotify.artists.map(artist => {
         return {
             name: artist.name,
-            url: artist.external_urls.spotify
+            url: artist.external_urls.spotify,
+            uri: artist.uri,
+            id: artist.id,
         };
     });
+
+    // send off the data
     res.send({
+        name: spotify.name,
+        url: spotify.external_urls.spotify,
+        uri: spotify.uri,
+        preview: spotify.preview_url,
+        id: spotify.id,
+        duration: ms(spotify.duration_ms, { long: true }),
+        trackNumber: spotify.track_number,
         album: {
-            name: spotify.tracks.items[0].album.name,
-            url: spotify.tracks.items[0].album.external_urls.spotify
-        },
-        song: {
-            name: spotify.tracks.items[0].name,
-            url: spotify.tracks.items[0].external_urls.spotify,
-            preview: spotify.tracks.items[0].preview_url,
-            nowPlaying: lastfm["@attr"] === undefined ? false : true
+            name: spotify.album.name,
+            url: spotify.album.external_urls.spotify,
+            uri: spotify.album.uri,
+            id: spotify.album.id,
+            images: spotify.album.images,
+            releaseDate: spotify.album.release_date,
+            trackCount: spotify.album.total_tracks,
         },
         artists
     });
